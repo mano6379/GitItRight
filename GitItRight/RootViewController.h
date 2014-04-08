2925742bf254b39c7a34eda564a47d6b575c3614
Marion Ano <meaola@gmail.com>
2014-04-07 20:17:49 -0500

    second commit

---
+++ GitItRight/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  GitItRight
+//
+//  Created by Marion Ano on 4/7/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
