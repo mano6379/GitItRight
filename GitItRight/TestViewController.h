7c7332e652129ed6ed4817a25dfbcf8097a1adec
Marion Ano <meaola@gmail.com>
2014-04-07 19:49:10 -0500

    created custom viewcontroller

---
+++ GitItRight/TestViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestViewController.h
+//  GitItRight
+//
+//  Created by Marion Ano on 4/7/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestViewController : UIViewController
+
+@end
