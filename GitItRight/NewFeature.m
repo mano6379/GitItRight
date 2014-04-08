d7b8c3d46c7d4da139e4dc5ede0f35a62191242d
dkw5877 <dkw5877@gmail.com>
2014-04-07 19:48:06 -0500

    new feature added

---
+++ GitItRight/NewFeature.m
@@ -0,0 +1,13 @@
+//
+//  NewFeature.m
+//  GitItRight
+//
+//  Created by user on 4/7/14.
+//  Copyright (c) 2014 Marion Ano. All rights reserved.
+//
+
+#import "NewFeature.h"
+
+@implementation NewFeature
+
+@end
